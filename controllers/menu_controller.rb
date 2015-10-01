92d1a6b81e44a20ce7088e5b2ab7aa929e15fbfb
carlsbad505 <carl@tabrific.com>
2015-10-01 04:26:23 +0000

    Attempt Assignment

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
     def main_menu
         puts "Main Menu - #{@address_book.entries.count} entries"
         puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - View entry by number"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         print "Enter your selection: "
         
         selection = gets.to_i
@@ -26,17 +27,21 @@ class MenuController
           main_menu
         when 2
           system "clear"
-          create_entry
+          view_by_num
           main_menu
         when 3
           system "clear"
-          search_entry
+          create_entry
           main_menu
         when 4
           system "clear"
-          read_csv
+          search_entry
           main_menu
         when 5
+          system "clear"
+          read_csv
+          main_menu
+        when 6
           puts "Goodbye!"
           exit(0)
         else
@@ -54,6 +59,20 @@ class MenuController
         end
         system "clear"
         puts "End of Entries"
+    end
+    
+    def view_by_num
+        system "clear"
+        print "Enter your entry number: "
+        selection = gets.to_i
+        @address_book.fetch(selection) do |x|
+            if x = nil
+              return "No entry found"
+            else
+              puts x.to_s
+            end
+            entry_submenu(entry)
+        end
     end
     
     def create_entry
