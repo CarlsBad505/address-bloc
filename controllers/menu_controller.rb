7c073b9f776af42d293dda26b698622a86b07818
carlsbad505 <carl@tabrific.com>
2015-10-02 18:45:36 +0000

    Finish Address Program

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -94,9 +94,34 @@ class MenuController
     end
     
     def search_entry
+      print "Search by Name: "
+      name = gets.chomp
+      match = @address_book.binary_search(name)
+      if match
+        puts match.to_s # <---- Get rid of to_s and see what happens
+        search_submenu
+      else
+        puts "No match found for #{name}"
+      end
     end
     
     def read_csv
+      print "Enter CSV file to be imported: "
+      file_name = gets.chomp
+       if file_name.empty?
+         system "clear"
+         puts "No CSV File"
+         main_menu
+       end
+       
+       begin
+         entry_count = @address.import_from_csv(file_name).count
+         system "clear"
+         puts "#{entry_count} entries have been added from #{file_name}"
+       rescue
+         puts "#{file_name} is invalid, please try again."
+         read_csv
+       end
     end
     
     def entry_submenu(entry)
@@ -109,7 +134,11 @@ class MenuController
         case selection
           when "n"
           when "d"
+            delete_entry(entry)
+            main menu
           when "e"
+            edit_entry(entry)
+            entry_submenu(entry)
           when "m"
             system "clear"
             main_menu
@@ -118,5 +147,51 @@ class MenuController
             puts "#{selection} is invalid"
             entry_submenu(entry)
         end
+    end
+    
+    def delete_entry(entry)
+      @address_book.entries.delete(entry)
+      puts "#{entry} has been deleted"
+    end
+    
+    def edit_entry(entry)
+      print "Updated Name: "
+      name = gets.chomp
+      print "Updated Phone Number: "
+      phone_number = gets.chomp
+      print "Updated Email: "
+      email = gets.chomp
+      entry.name = name if !name.empty?
+      entry.phone_number = phone_number if !phone_number.empty?
+      entry.email = email if !email.empty?
+      system "clear"
+      puts "Updated Entry:"
+      puts entry
+    end
+    
+    def search_submenu(entry)
+      puts "\nd - delete entry"
+      puts "e - edit entry"
+      puts "m - return to main menu"
+      selection = gets.chomp
+      
+      case selection
+        when "d"
+          system "clear"
+          delete_entry(entry)
+          main_menu
+        when "e"
+          edit_entry(entry)
+          system "clear"
+          main_menu
+        when "m"
+          system "clear"
+          main_menu
+        else
+          system "clear"
+          puts "#{selection} is invalid, please try again"
+          puts entry
+          search_submenu(entry)
+      end
     end
 end
